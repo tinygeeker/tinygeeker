94a8eb5453874b80558bfbf76218ccde379660b9
cxyliangzai <cxyliangzai@163.com>
2022-06-16 21:15:48 +0800

    个人存储库

--- assets/js/github.js
+++
@@ -1,6 +0,0 @@
-const config = require('./config')
-const { getNPMPackages, getNPMPackagesDownloads } = require('./axios')
-
-exports.getGitHubStars = async () => {
-  return 0
-}
--- assets/js/utils.js
+++
@@ -1,7 +0,0 @@
-exports.JSONStringify = (data) => JSON.stringify(data, null, 2)
-
-exports.thousands = (num) => {
-  var str = num.toString()
-  var reg = str.indexOf('.') > -1 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(?:\d{3})+$)/g
-  return str.replace(reg, '$1,')
-}
